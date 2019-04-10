2d178cf5c75661f325d703077cd358a74fddfb6d
Henry Koehler <henryk1229@gmail.com>
2019-04-10 19:04:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ def word_substituter(tweet)
   dictionary.each do |key, value|
     array.map { |string| string == key.to_s ? string == value : string }
   end
-  array.to_s 
+  array 
 end
 
     
