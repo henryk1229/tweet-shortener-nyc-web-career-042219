344b9ac583ee28e5a4ecab60cc810103e530a7c7
Henry Koehler <henryk1229@gmail.com>
2019-04-10 20:55:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,9 +31,13 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  tweet.length > 140 ? word_substituter(tweet)[0.136]<<"..." : tweet 
+  if tweet.length > 140
+    selective_tweet_shortener(tweet) 
+    tweet[0..136]<<"..." 
+  else
+    tweet
+  end
   
-  #selective_tweet_shortener(tweet) ? tweet[0..136]<<"..." : tweet 
 end
 
 
